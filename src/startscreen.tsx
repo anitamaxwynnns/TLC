539ad3126b8f8bb901fa59bbd99ce590de92dfea
JOSHUA-CHUA-12 <jchua378@gmail.com>
2024-05-29 23:07:20 +0800

    start screen with background

--- src/startscreen.tsx
+++ src/startscreen.tsx
@@ -1,5 +1,20 @@
-import { View, Text, Image, Button } from 'react-native'
+import { View, Text, ImageBackground, Button, StyleSheet } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
+export default function startscreen() {
+  return (
+    <View>
+      <ImageBackground source={require('../assets/background.png')} style={styles.image} >
+      </ImageBackground>
+    </View>
+  )
+}
 
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  }
+})
 
