436b82e393394928a6903ed1281f9d6213653ff8
JOSHUA-CHUA-12 <jchua378@gmail.com>
2024-05-29 23:15:29 +0800

    start screen with background

--- App.tsx
+++ App.tsx
@@ -1,15 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View } from 'react-native'
+import StartScreen from './src/startscreen'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <StartScreen />
+  )
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
