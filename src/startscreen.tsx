708bec85a2a8540121b456e98ef9ce79656ac702
JOSHUA-CHUA-12 <jchua378@gmail.com>
2024-05-30 11:06:30 +0800

    final update for background startscreen

--- src/startscreen.tsx
+++ src/startscreen.tsx
@@ -1,12 +1,16 @@
-import { View, Text, ImageBackground, Button, StyleSheet } from 'react-native'
+import { View, Text, ImageBackground, Button, StyleSheet, Image } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 export default function StartScreen() {
   return (
-    <View style={styles.container}>
-      <ImageBackground source={require('../assets/background.png')} style={styles.image} >
-      </ImageBackground>
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <ImageBackground source={require('../assets/background.png')} style={styles.image}>
+          <Image source={require('../assets/background2.png')} style={styles.image2}></Image>
+          <Text style={styles.text}>{"Track.Lift.Connect"}</Text>
+        </ImageBackground>
+      </View>
+    </SafeAreaProvider>
   )
 }
 
@@ -14,11 +18,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
+    alignItems: 'center',
   },
   image: {
     flex: 1,
     resizeMode: 'cover',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image2: {
+    justifyContent: 'center',
+    resizeMode: 'contain',
+    marginRight: 18,
+    marginLeft: 1,
+  },
+  text: {
+    fontSize: 50,
   }
 })
 
