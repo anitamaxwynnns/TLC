e8b340dfa9c3e9f3bf7cb5fc4ce47cb3efa9b73b
JOSHUA-CHUA-12 <jchua378@gmail.com>
2024-05-30 19:02:14 +0800

    App route startscreen to signup screen

--- App.tsx
+++ App.tsx
@@ -1,15 +1,27 @@
 import { StyleSheet, Text, View } from 'react-native'
-import StartScreen from './src/startscreen'
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+import StartScreen from './src/StartScreen'
+import Signup from './src/signup'
+type RootStackNavigatorParamsList = {
+  StartScreen: undefined
+  SignUp: undefined
+}
+
+const Stack = createStackNavigator<RootStackNavigatorParamsList>()
+
 export default function App() {
   return (
-    <StartScreen />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='StartScreen'>
+        <Stack.Screen name='StartScreen' component={StartScreen} options={{ headerShown: false }} />
+        <Stack.Screen name='SignUp' component={Signup} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+
+
