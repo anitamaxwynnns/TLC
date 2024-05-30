3c3e3a2cf7a316e70454ac4d123ba8d84fccde46
JOSHUA-CHUA-12 <jchua378@gmail.com>
2024-05-30 21:30:54 +0800

    signup components w/o backend

--- src/signup.tsx
+++ src/signup.tsx
@@ -1,13 +1,21 @@
-import { View, Text, ImageBackground, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, ImageBackground, StyleSheet, Image } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { Button, TextInput } from 'react-native-paper'
 import { supabase } from './supabase'
 
 export default function Signup() {
+  const [text, setText] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState('')
   return (
     <SafeAreaProvider>
       <View style={styles.container}>
-        <ImageBackground source={require('../assets/background.png')} style={styles.image} blurRadius={2}></ImageBackground>
+        <ImageBackground source={require('../assets/background.png')} style={styles.image} blurRadius={2}>
+          <TextInput mode='outlined' label='Email Address' value={text} onChangeText={text => setText(text)} activeOutlineColor='black' style={styles.textinput} />
+          <TextInput mode='outlined' label='Password' value={text} onChangeText={text => setText(text)} activeOutlineColor='black' style={styles.textinput2} />
+          <Button mode='contained' theme={{ colors: { primary: 'black' } }} style={styles.button}>{'Submit'}</Button>
+        </ImageBackground>
       </View>
     </SafeAreaProvider>
   )
@@ -21,5 +29,21 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
     resizeMode: 'contain',
+    alignItems: 'center',
+  },
+  textinput: {
+    marginTop: 400,
+    justifyContent: 'center',
+    width: 300,
+    marginRight: 20,
+  },
+  textinput2: {
+    marginRight: 20,
+    width: 300,
+    marginTop: 30,
+  },
+  button: {
+    marginTop: 30,
+    marginRight: 15,
   }
 })
