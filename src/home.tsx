56615d04e7a4b45066269c937f8b9403ca8bf03c
JOSHUA-CHUA-12 <jchua378@gmail.com>
2024-06-01 22:16:54 +0800

    original state for home and main

--- src/home.tsx
+++ src/home.tsx
@@ -1,3 +1,20 @@
+import { supabase } from "./supabase";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+async function getExercises() {
+  const { data, error } = await supabase
+    .from("exercise_table")
+    .select()
+
+  if (error) {
+    console.error("Error", error)
+    return []
+  }
+  console.log(data)
+  return data
+}
+
+
 export default function Home() {
-  return 5
+  return (getExercises())
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { View, Text } from 'react-native'
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Home from './home'
+import Ionicons from '@expo/vector-icons'
 const Tab = createBottomTabNavigator()
 
 export default function Main() {
