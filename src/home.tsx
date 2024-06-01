8bbc20896927bce488e22b39ee6609b6cb563bcd
JOSHUA-CHUA-12 <jchua378@gmail.com>
2024-06-01 23:53:44 +0800

    display exercises flatlist

--- src/home.tsx
+++ src/home.tsx
@@ -1,20 +1,36 @@
 import { supabase } from "./supabase";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { getExercises } from "./db";
+import { useState, useEffect } from "react";
+import { FlatList, View, Text } from "react-native"
 
-async function getExercises() {
-  const { data, error } = await supabase
-    .from("exercise_table")
-    .select()
-
-  if (error) {
-    console.error("Error", error)
-    return []
-  }
-  console.log(data)
-  return data
+function ExerciseComponent({ exercise }: { exercise: any }) {
+  return <View>
+    <Text>{exercise.name}</Text>
+    <Text>{exercise.muscle}</Text>
+  </View>
 }
-
 
 export default function Home() {
-  return (getExercises())
+  const [exercises, setExercises] = useState<any[]>()
+
+  useEffect(() => {
+    let ignore = false
+    getExercises().then(result => {
+      if (!ignore) {
+        setExercises(result)
+      }
+    })
+    return () => {
+      ignore = true
+    }
+  }, [])
+
+  return (
+    <SafeAreaView>
+      <FlatList data={exercises} renderItem={({ item }) => <ExerciseComponent exercise={item} />} keyExtractor={item => item.name} />
+    </SafeAreaView>
+  )
 }
+
+
