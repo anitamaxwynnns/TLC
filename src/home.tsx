985bcebe2c5303eb139ff8b64591795056b5eac3
JOSHUA-CHUA-12 <jchua378@gmail.com>
2024-06-02 16:13:07 +0800

    styling for home page

--- src/home.tsx
+++ src/home.tsx
@@ -1,12 +1,14 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { getExercises } from "./db";
 import { useState, useEffect } from "react";
-import { FlatList, View, Text } from "react-native"
+import { FlatList, View, Text, StyleSheet } from "react-native"
 
 function ExerciseComponent({ exercise }: { exercise: any }) {
-  return <View style={{ gap: 4 }}>
-    <Text style={{ fontWeight: "bold" }}>{exercise.name}</Text>
-    <Text>{exercise.muscle}</Text>
+  return <View style={styles.itemContainer}>
+    <View style={styles.textContainer}>
+      <Text style={styles.exerciseName}>{exercise.name}</Text>
+      <Text style={styles.bodyPart}>{exercise.muscle}</Text>
+    </View>
   </View>
 }
 
@@ -31,3 +33,27 @@ export default function Home() {
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    flexDirection: 'row',
+    padding: 10,
+    alignItems: 'center',
+    flex: 1
+  },
+  image: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  textContainer: {
+    flexDirection: 'column',
+  },
+  exerciseName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  bodyPart: {
+    fontSize: 14,
+  },
+});
