3ad53253978bae4a0824e1ec6e358f7de8d20444
JOSHUA-CHUA-12 <jchua378@gmail.com>
2024-06-06 14:53:09 +0800

    connect image picker to supabase

---
+++ src/avatar.tsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react'
+import { supabase } from './supabase';
+import { StyleSheet, View, Alert, Image, Button } from 'react-native';
+import * as ImagePicker from 'expo-image-picker'
+
+interface Props {
+  size: number
+  url: string | null
+  onUpload: (filePath: string) => void
+}
+
+export default function Avatar({ url, size = 150, onUpload }: Props) {
+  const [uploading, setUploading] = useState(false)
+  const [avatarUrl, setAvatarUrl] = useState<string | null>(null)
+  const avatarSize = { height: size, width: size }
+
+  useEffect(() => {
+    if (url) downloadImage(url)
+  }, [url])
+
+  async function downloadImage(path: string) {
+    try {
+      const { data, error } = await supabase.storage.from('avatars').download(path)
+
+      if (error) {
+        throw error
+      }
+
+      const fr = new FileReader()
+      fr.readAsDataURL(data)
+      fr.onload = () => {
+        setAvatarUrl(fr.result as string)
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log('Error downloading image: ', error.message)
+      }
+    }
+  }
+
+
+
+
+}
