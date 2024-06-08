cbb3a69f8d1ad207d4796769a86ee92ede536d76
JOSHUA-CHUA-12 <jchua378@gmail.com>
2024-06-08 13:58:51 +0800

    useAuth() hook

--- src/auth_provider.tsx
+++ src/auth_provider.tsx
@@ -1,22 +1,32 @@
-import { createContext, useEffect, useState } from "react"
-import { supabase } from "./supabase"
-import { Session } from "@supabase/supabase-js"
+import { ReactNode, createContext, useContext, useEffect, useState } from "react";
+import { supabase } from "./supabase";
+import { Session } from "@supabase/supabase-js";
 
-const initialState = { session: null, user: null}
-export const AuthContext = createContext(initialState)
+const initialState: { session: Session | null } = { session: null };
+export const AuthContext = createContext(initialState);
 
-export default function AuthProvider({children}) {
-  const [session, setSession] = useState<Session|null>(null)
+export default function AuthProvider({ children }: { children: ReactNode }) {
+    const [session, setSession] = useState<Session | null>(null);
 
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+            setSession(session);
+        });
 
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
-    return <AuthContext.Provider value={session}>{children}</AuthContext.Provider>
+        supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session);
+        });
+    }, []);
+    return (
+        <AuthContext.Provider value={{ session: session }}>
+            {children}
+        </AuthContext.Provider>
+    );
 }
-    
+export function useAuth() {
+    const context = useContext(AuthContext)
+    if (context === undefined) {
+        throw Error ('useAuth must be used within AuthProvider')
+    }
+    return context
+}
