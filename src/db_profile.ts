157ad967d5adf73be446b4bc28e7b25e4e9e6790
JOSHUA-CHUA-12 <jchua378@gmail.com>
2024-06-08 14:01:37 +0800

    import auth hook

--- src/db_profile.ts
+++ src/db_profile.ts
@@ -1,4 +1,5 @@
 import { supabase } from "./supabase";
+import { useAuth } from "./auth_provider";
 
 export async function getUserInfo(): Promise<{ name: string } | undefined> {
 	const { data, error } = await supabase.from("profile").select("name")
