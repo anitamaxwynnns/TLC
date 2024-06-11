4a7509b4a18d28fc6a5ff0b482f417979ea710d4
JOSHUA-CHUA-12 <jchua378@gmail.com>
2024-06-11 16:03:45 +0800

    change database format for sectionlist

--- src/db.ts
+++ src/db.ts
@@ -1,14 +1,35 @@
-import { supabase } from "./supabase"
+import { supabase } from "./supabase";
 
 export async function getExercises() {
-	const { data, error } = await supabase
-		.from("exercise_table")
-		.select("name,muscle")
+    const { data, error } = await supabase
+        .from("exercise_table")
+        .select("name,muscle,first_letter");
 
+    console.log(data);
 
-	if (error) {
-		console.error(error)
-		return []
-	}
-	return data
+    if (error) {
+        console.error(error);
+        return [];
+    }
+    interface Exercise {
+        name: string;
+        muscle: string;
+        first_letter: string;
+    }
+    interface Section {
+        title: string;
+        data: Exercise[];
+    }
+    const sections = data.reduce<Section[]>((accumulator, current) => {
+        const firstLetter = current.first_letter;
+        let section = accumulator.find(s => s.title === firstLetter);
+        if (!section) {
+            section = { title: firstLetter, data: [] };
+            accumulator.push(section);
+        }
+        section.data.push(current);
+        return accumulator;
+    }, [] as Section[]);
+
+    return sections;
 }
