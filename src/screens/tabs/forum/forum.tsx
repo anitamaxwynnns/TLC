03f0b1f7820497a1bc94fcede36271bb141d1576
JOSHUA-CHUA-12 <jchua378@gmail.com>
2024-06-20 14:38:27 +0800

    forum frontend

--- src/screens/tabs/forum/forum.tsx
+++ src/screens/tabs/forum/forum.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {
     View,
@@ -7,13 +7,16 @@ import {
     StyleSheet,
     Alert,
     AppState,
+    FlatList,
+    Button,
+    Image,
 } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { Button, TextInput } from "react-native-paper";
-import { supabase } from "./supabase";
-import { useNavigation } from "@react-navigation/native";
+import {  TextInput } from "react-native-paper";
+import { supabase } from "src/libs/database/supabase";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
-import { RootStackNavigatorParamsList } from "../App";
+import { RootStackNavigatorParamsList } from "App";
 
 type Post = {
     id: string;
@@ -25,7 +28,48 @@ type Post = {
     };
 };
 
-export default function Forum() {}
+export default function Forum() {
+    const navigation =
+        useNavigation<NavigationProp<RootStackNavigatorParamsList>>();
+    const [posts, setPosts] = useState<Post[]>([]);
+
+    useEffect(() => {
+        fetchPosts();
+    }, []);
+
+    const fetchPosts = async () => {
+        let {data: Post, error } = await supabase.from('posts').select('id, image_url, caption, likes, profile (name)');
+        if (error) console.log('error', error);
+        else setPosts(posts);
+    };
+
+    const likePosts = async (postId: string, currentLikes: number) => {
+        let { error } = await supabase.from('posts').update({ likes: currentLikes + 1 })
+        .eq('id', postId);
+    if (!error) fetchPosts();
+    }
+    
+     return (
+    <View>
+      <Button title="Create Post" onPress={() => navigation.navigate('CreatePost')} />
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View>
+            <Image source={{ uri: item.image_url }} style={{ width: 100, height: 100 }} />
+            <Text>{item.caption}</Text>
+            <Text>Posted by: {item.profile.name}</Text>
+            <Button title="Like" onPress={() => likePosts(item.id, item.likes)} />
+            <Text>{item.likes} Likes</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+
+
+}
 
 const styles = StyleSheet.create({
     container: {
