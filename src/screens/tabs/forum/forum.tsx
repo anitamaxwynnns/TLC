42f7eb5a001ec94d2fe74a7fa9350005b06bf6b6
JOSHUA-CHUA-12 <jchua378@gmail.com>
2024-06-20 11:47:20 +0800

    type for posts

--- src/screens/tabs/forum/forum.tsx
+++ src/screens/tabs/forum/forum.tsx
@@ -1,26 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { View, Text, ImageBackground, StyleSheet, Alert, AppState } from 'react-native'
+import {
+    View,
+    Text,
+    ImageBackground,
+    StyleSheet,
+    Alert,
+    AppState,
+} from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Button, TextInput } from "react-native-paper";
+import { supabase } from "./supabase";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootStackNavigatorParamsList } from "../App";
 
+type Post = {
+    id: string;
+    image_url: string;
+    caption: string;
+    likes: number;
+    profile: {
+        name: string;
+    };
+};
 
-export default function Forum() {
-    return(
-        <SafeAreaView style={styles.container}>
-        <View>
-            <Text style={styles.placeholderText}>Forum</Text>
-        </View>
-        </SafeAreaView>
-    )
-}
+export default function Forum() {}
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
     },
     placeholderText: {
-      fontSize: 30,
-      fontWeight: 'bold',
+        fontSize: 30,
+        fontWeight: "bold",
     },
-  });
+});
