d2668a1706145731abe120d202c1f4472d169327
JOSHUA-CHUA-12 <jchua378@gmail.com>
2024-06-27 11:10:16 +0800

    image selector v1

--- src/screens/tabs/forum/ImageSelector.tsx
+++ src/screens/tabs/forum/ImageSelector.tsx
@@ -1,17 +1,38 @@
 import { NavigationProp, useNavigation } from "@react-navigation/native";
 import { RootStackNavigatorParamsList } from "App";
+import { useState } from "react";
 import { SafeAreaView, Text, View } from "react-native";
 import { Appbar, Button } from "react-native-paper";
+import ImageSelectorProp from "./ImageSelectorProp";
 
 export default function ImageSelector() {
     const navigation =
         useNavigation<NavigationProp<RootStackNavigatorParamsList>>();
+    const [avatarUrl, setAvatarUrl] = useState("");
     return (
-        <View style={{flex:1}}>
-        <Appbar.Header>
-            <Appbar.Content title='Select Image' />
-        </Appbar.Header>
-            <Button onPress={() => navigation.navigate('CreatePost')}>Next</Button>
+        <View style={{ flex: 1 }}>
+            <Appbar.Header>
+                <Appbar.Content title="Select Image" />
+            </Appbar.Header>
+            <ImageSelectorProp
+                size={500}
+                url={avatarUrl}
+                onUpload={(url: string) => {
+                    setAvatarUrl(url);
+                }}
+            />
+            <Button
+                onPress={() => navigation.navigate("CreatePost")}
+                style={{
+                    width: 80,
+                    alignSelf: "center",
+                    backgroundColor: "black",
+                    marginTop: 50,
+                }}
+                textColor="white"
+            >
+                Next
+            </Button>
         </View>
     );
 }
