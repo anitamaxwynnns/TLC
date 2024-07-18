3a0b91d00929a67a9e750fda88c91b3d1159adf9
JOSHUA-CHUA-12 <jchua378@gmail.com>
2024-07-18 23:27:22 +0800

    i cooked

--- src/screens/tabs/workout/track-workout.tsx
+++ src/screens/tabs/workout/track-workout.tsx
@@ -1,5 +1,5 @@
 import { NavigationProp, useNavigation } from "@react-navigation/native";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { FlatList, Pressable, SafeAreaView, Text, View } from "react-native";
 import { getOneWorkout } from "src/libs/database/functions";
 import { RootStackNavigatorParamsList } from "./workout";
@@ -18,9 +18,11 @@ type FormattedExercise = {
 
 function RenderItem({
     item,
+    exerciseIndex,
     checkSet,
 }: {
     item: FormattedExercise;
+    exerciseIndex: number;
     checkSet: any;
 }) {
     return (
@@ -41,42 +43,47 @@ function RenderItem({
                     borderRadius: 10,
                 }}
             >
-                <View style={{ gap: 5 }}>
-                    <Text style={{ fontSize: 24, fontWeight: 500 }}>Sets</Text>
-                    {Array.from({ length: item.sets }, (_, i) => (
-                        <View key={`${index}-${i}`} style={{}}>
-                            <Text style={{ fontSize: 24 }}>{i + 1}</Text>
-                        </View>
-                    ))}
-                </View>
-                <View style={{ gap: 5 }}>
-                    <Text style={{ fontSize: 24, fontWeight: 500 }}>Reps</Text>
-                    {Array.from({ length: item.sets }, (_, i) => (
-                        <View key={`${index}-${i}`} style={{}}>
-                            <Text style={{ fontSize: 24 }}>{item.reps}</Text>
-                        </View>
-                    ))}
-                </View>
-                <View style={{ marginTop: 5 }}>
-                    <Entypo
-                        name="check"
-                        size={24}
-                        color="black"
-                        style={{ marginLeft: 5 }}
-                    />
-                    {Array.from({ length: item.sets }, (_, i) => (
-                        <View
-                            key={`${index}-${i}`}
-                            style={{ transform: [{ scale: 1.25 }] }}
-                        >
+                <Text style={{ fontSize: 24, fontWeight: 500 }}>Sets</Text>
+                <Text style={{ fontSize: 24, fontWeight: 500 }}>Reps</Text>
+                <Entypo
+                    name="check"
+                    size={24}
+                    color="black"
+                    style={{ marginLeft: 5 }}
+                />
+            </View>
+            {item.sets.map((set, setIndex) => (
+                <View
+                    key={`${exerciseIndex}:${setIndex}`}
+                    style={{
+                        flexDirection: "row",
+                        justifyContent: "space-around",
+                        gap: 60,
+                        borderWidth: 2,
+                        padding: 10,
+                        borderColor: "grey",
+                        borderRadius: 10,
+                        alignItems: "center",
+                    }}
+                >
+                    <View style={{ gap: 5 }}>
+                        <Text>{set.set}</Text>
+                    </View>
+                    <View style={{ gap: 5 }}>
+                        <Text>{set.reps}</Text>
+                    </View>
+                    <View style={{ marginTop: 5 }}>
+                        <View style={{ transform: [{ scale: 1.25 }] }}>
                             <Checkbox.Android
-                                status={checked ? "checked" : "unchecked"}
-                                onPress={() => setChecked(!checked)}
+                                status={set.checked ? "checked" : "unchecked"}
+                                onPress={() =>
+                                    checkSet(exerciseIndex, setIndex)
+                                }
                             />
                         </View>
-                    ))}
+                    </View>
                 </View>
-            </View>
+            ))}
         </View>
     );
 }
@@ -115,12 +122,20 @@ export default function TrackWorkout({ route }: any) {
             }
         });
     }, [workoutId]);
+    const isFinished = useMemo(
+        () =>
+            workouts.every((exercise) =>
+                exercise.sets.every((set) => set.checked),
+            ),
+        [workouts],
+    );
 
     function checkSet(exerciseIndex: number, setIndex: number) {
         const updated: FormattedExercise[] = JSON.parse(
             JSON.stringify(workouts),
         );
-        updated[exerciseIndex].sets[setIndex].checked = true;
+        updated[exerciseIndex].sets[setIndex].checked =
+            !updated[exerciseIndex].sets[setIndex].checked;
         setWorkouts(updated);
     }
 
@@ -134,45 +149,39 @@ export default function TrackWorkout({ route }: any) {
             <FlatList
                 data={workouts}
                 contentContainerStyle={{ gap: 20 }}
-                renderItem={({ item }) => (
-                    <RenderItem item={item} checkSet={checkSet} />
+                renderItem={({ item, index }) => (
+                    <RenderItem
+                        item={item}
+                        exerciseIndex={index}
+                        checkSet={checkSet}
+                    />
                 )}
                 keyExtractor={(_, index) => `${index}`}
+                ListFooterComponent={
+                    <View style={{ paddingBottom: 20, paddingHorizontal: 20 }}>
+                        <Pressable
+                            disabled={!isFinished}
+                            style={{
+                                backgroundColor: isFinished ? "black" : "gray",
+                                borderRadius: 200,
+                                padding: 20,
+                                width: "100%",
+                            }}
+                        >
+                            <Text
+                                style={{
+                                    color: "white",
+                                    alignSelf: "center",
+                                    fontSize: 18,
+                                    fontWeight: 700,
+                                }}
+                            >
+                                Finish Workout
+                            </Text>
+                        </Pressable>
+                    </View>
+                }
             />
         </SafeAreaView>
     );
 }
-
-// export default function TrackWorkout({ route }: any) {
-//     const navigation =
-//         useNavigation<NavigationProp<RootStackNavigatorParamsList>>();
-//     const [workouts, setWorkouts] = useState<any>([]);
-//     const { workoutId } = route.params;
-//     useEffect(() => {
-//         let ignore = false;
-//         getOneWorkout(workoutId).then((result) => {
-//             if (!ignore) {
-//                 if (result !== undefined) {
-//                     setWorkouts(result);
-//                 }
-//             }
-//         });
-//         console.log(workouts);
-//     }, [workoutId]);
-//
-//     return (
-//         <SafeAreaView style={{ flex: 1 }}>
-//             <View style={{ padding: 20 }}>
-//                 <Pressable onPress={() => navigation.goBack()}>
-//                     <Text style={{ fontSize: 15, fontWeight: 500 }}>Back</Text>
-//                 </Pressable>
-//             </View>
-//             <FlatList
-//                 data={workouts.workout_exercise}
-//                 contentContainerStyle={{ gap: 20 }}
-//                 renderItem={({ item }) => <RenderItem item={item} />}
-//                 keyExtractor={(_, index) => `${index}`}
-//             />
-//         </SafeAreaView>
-//     );
-// }
