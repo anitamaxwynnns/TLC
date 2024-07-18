37c309b33b7ec2885b40ccc81746fe069c95092c
JOSHUA-CHUA-12 <jchua378@gmail.com>
2024-07-18 23:09:17 +0800

    fibbles

--- src/screens/tabs/workout/track-workout.tsx
+++ src/screens/tabs/workout/track-workout.tsx
@@ -4,57 +4,75 @@ import { FlatList, Pressable, SafeAreaView, Text, View } from "react-native";
 import { getOneWorkout } from "src/libs/database/functions";
 import { RootStackNavigatorParamsList } from "./workout";
 import { Entypo } from "@expo/vector-icons";
+import { Checkbox } from "react-native-paper";
 
-type Exercises = {
-    exercise_id: number;
-    sets: number;
-    reps: number;
+type FormattedExercise = {
+    name: string;
+    muscle: string;
+    sets: {
+        set: number;
+        reps: number;
+        checked: boolean;
+    }[];
 };
 
-function renderItem({
+function RenderItem({
     item,
-    index,
+    checkSet,
 }: {
-    item: {
-        sets: any;
-        reps: any;
-        exercise_table: {
-            name: any;
-            muscle: any;
-        };
-    };
-    index: number;
+    item: FormattedExercise;
+    checkSet: any;
 }) {
     return (
-        <View key={index} style={{ gap: 10, padding: 10 }}>
-            <View>
-                <Text style={{ fontSize: 20, fontWeight: 500 }}>
-                    {item.exercise_table.name}
+        <View style={{ gap: 10, padding: 10 }}>
+            <View style={{ paddingLeft: 10 }}>
+                <Text style={{ fontSize: 25, fontWeight: 500 }}>
+                    {item.name}
                 </Text>
             </View>
             <View
-                style={{ flexDirection: "row", justifyContent: "space-evenly" }}
+                style={{
+                    flexDirection: "row",
+                    justifyContent: "space-around",
+                    gap: 60,
+                    borderWidth: 2,
+                    padding: 10,
+                    borderColor: "grey",
+                    borderRadius: 10,
+                }}
             >
-                <View style={{ alignItems: "center", gap: 5 }}>
-                    <Text style={{ fontSize: 18, fontWeight: 500 }}>Sets</Text>
+                <View style={{ gap: 5 }}>
+                    <Text style={{ fontSize: 24, fontWeight: 500 }}>Sets</Text>
                     {Array.from({ length: item.sets }, (_, i) => (
                         <View key={`${index}-${i}`} style={{}}>
-                            <Text style={{ fontSize: 15 }}>{i + 1}</Text>
+                            <Text style={{ fontSize: 24 }}>{i + 1}</Text>
                         </View>
                     ))}
                 </View>
-                <View style={{ alignItems: "center", gap: 5 }}>
-                    <Text style={{ fontSize: 18, fontWeight: 500 }}>Reps</Text>
+                <View style={{ gap: 5 }}>
+                    <Text style={{ fontSize: 24, fontWeight: 500 }}>Reps</Text>
                     {Array.from({ length: item.sets }, (_, i) => (
                         <View key={`${index}-${i}`} style={{}}>
-                            <Text style={{ fontSize: 15 }}>{item.reps}</Text>
+                            <Text style={{ fontSize: 24 }}>{item.reps}</Text>
                         </View>
                     ))}
                 </View>
-                <View style={{ alignItems: "center", gap: 5 }}>
-                    <Entypo name="check" size={24} color="black" />
+                <View style={{ marginTop: 5 }}>
+                    <Entypo
+                        name="check"
+                        size={24}
+                        color="black"
+                        style={{ marginLeft: 5 }}
+                    />
                     {Array.from({ length: item.sets }, (_, i) => (
-                        <View key={`${index}-${i}`} style={{}}>
+                        <View
+                            key={`${index}-${i}`}
+                            style={{ transform: [{ scale: 1.25 }] }}
+                        >
+                            <Checkbox.Android
+                                status={checked ? "checked" : "unchecked"}
+                                onPress={() => setChecked(!checked)}
+                            />
                         </View>
                     ))}
                 </View>
@@ -66,19 +84,45 @@ function renderItem({
 export default function TrackWorkout({ route }: any) {
     const navigation =
         useNavigation<NavigationProp<RootStackNavigatorParamsList>>();
-    const [workouts, setWorkouts] = useState<any>([]);
+    const [workouts, setWorkouts] = useState<FormattedExercise[]>([]);
     const { workoutId } = route.params;
     useEffect(() => {
         let ignore = false;
         getOneWorkout(workoutId).then((result) => {
             if (!ignore) {
                 if (result !== undefined) {
-                    setWorkouts(result);
+                    const formatted: FormattedExercise[] =
+                        result.workout_exercise.map((val) => {
+                            const exercise: FormattedExercise = {
+                                name: val.exercise_table.name,
+                                muscle: val.exercise_table.muscle,
+                                sets: [],
+                            };
+                            for (let i = 1; i <= val.sets; ++i) {
+                                exercise.sets = [
+                                    ...exercise.sets,
+                                    {
+                                        set: i,
+                                        reps: val.reps,
+                                        checked: false,
+                                    },
+                                ];
+                            }
+                            return exercise;
+                        });
+                    setWorkouts(formatted);
                 }
             }
         });
-        console.log(workouts);
     }, [workoutId]);
+
+    function checkSet(exerciseIndex: number, setIndex: number) {
+        const updated: FormattedExercise[] = JSON.parse(
+            JSON.stringify(workouts),
+        );
+        updated[exerciseIndex].sets[setIndex].checked = true;
+        setWorkouts(updated);
+    }
 
     return (
         <SafeAreaView style={{ flex: 1 }}>
@@ -88,10 +132,47 @@ export default function TrackWorkout({ route }: any) {
                 </Pressable>
             </View>
             <FlatList
-                data={workouts.workout_exercise}
+                data={workouts}
                 contentContainerStyle={{ gap: 20 }}
-                renderItem={renderItem}
+                renderItem={({ item }) => (
+                    <RenderItem item={item} checkSet={checkSet} />
+                )}
+                keyExtractor={(_, index) => `${index}`}
             />
         </SafeAreaView>
     );
 }
+
+// export default function TrackWorkout({ route }: any) {
+//     const navigation =
+//         useNavigation<NavigationProp<RootStackNavigatorParamsList>>();
+//     const [workouts, setWorkouts] = useState<any>([]);
+//     const { workoutId } = route.params;
+//     useEffect(() => {
+//         let ignore = false;
+//         getOneWorkout(workoutId).then((result) => {
+//             if (!ignore) {
+//                 if (result !== undefined) {
+//                     setWorkouts(result);
+//                 }
+//             }
+//         });
+//         console.log(workouts);
+//     }, [workoutId]);
+//
+//     return (
+//         <SafeAreaView style={{ flex: 1 }}>
+//             <View style={{ padding: 20 }}>
+//                 <Pressable onPress={() => navigation.goBack()}>
+//                     <Text style={{ fontSize: 15, fontWeight: 500 }}>Back</Text>
+//                 </Pressable>
+//             </View>
+//             <FlatList
+//                 data={workouts.workout_exercise}
+//                 contentContainerStyle={{ gap: 20 }}
+//                 renderItem={({ item }) => <RenderItem item={item} />}
+//                 keyExtractor={(_, index) => `${index}`}
+//             />
+//         </SafeAreaView>
+//     );
+// }
